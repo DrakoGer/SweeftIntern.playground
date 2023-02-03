d87b0a3d30e18402a07923eb0800508428a4c1c2
DrakoGer <98773139+DrakoGer@users.noreply.github.com>
2023-02-04 01:31:31 +0400

    Update Contents.swift

--- SweeftIntern.playground/Contents.swift
+++ SweeftIntern.playground/Contents.swift
@@ -1,69 +1,77 @@
-//1.     დაწერეთ ფუნქცია, რომელსაც გადაეცემა ტექსტი  და აბრუნებს პალინდრომია თუ არა. (პალინდრომი არის ტექსტი რომელიც ერთნაირად იკითხება ორივე მხრიდან).
+import Foundation
 
-func isPalindrome(_ string: String) -> Bool {
-    let reversed = String(string.lowercased().reversed())
 
-    if string.lowercased() == reversed {
-        return true
-    } else {
-        return false
+//1. გვაქვს 1,5,10,20 და 50 თეთრიანი მონეტები. დაწერეთ ფუნქცია, რომელსაც გადაეცემა თანხა (თეთრებში) და აბრუნებს მონეტების მინიმალურ რაოდენობას, რომლითაც შეგვიძლია ეს თანხა დავახურდაოთ.
+
+func minimumCoins(amount: Int) -> Int {
+    let coins = [50, 20, 10, 5, 1]
+    var numCoins = 0
+    var amount = amount
+    for coin in coins {
+        numCoins += amount / coin
+        amount %= coin
     }
+    return numCoins
 }
 
-isPalindrome("Rotator")
-isPalindrome("Rats live on no evil star")
-isPalindrome("Hello, world")
+minimumCoins(amount: 100)
 
-//2. გვაქვს 1,5,10,20 და 50 თეთრიანი მონეტები. დაწერეთ ფუნქცია, რომელსაც გადაეცემა თანხა (თეთრებში) და აბრუნებს მონეტების მინიმალურ რაოდენობას, რომლითაც შეგვიძლია ეს თანხა დავახურდაოთ.
+//2. დაწერეთ ფუნქცია რომელიც დააჯამებს ციფრებს ორ რიცსხვს შორის.
+//   მაგალითად გადმოგვეცემა 19 და 22. მათ შორის ციფრების ჯამი იქნება :
+//   // 19, 20, 21, 22
+//   // (1 + 9) + (2 + 0) + (2 + 1) + (2 + 2) = 19
+func sumOfDigits(start: Int, end: Int) -> Int {
+    var sum = 0
+    for i in start...end {
+        let digits = String(i).map { Int(String($0))! }
+        sum += digits.reduce(0, +)
+    }
+    return sum
+}
 
-var coins = [1, 5, 10, 20, 50]
+sumOfDigits(start: 7, end: 8)
+//
+//   sumOfDigits(7, 8) ➞ 15
+//   sumOfDigits(17, 20) ➞ 29
+//   sumOfDigits(10, 12) ➞ 6
 
-func minSplit(_ money: Int, _ cents: [Int]) -> Int {
-    var centsNeed = [Int]()
-    var sum = money
+//3. მოცემულია String რომელიც შედგება „(" და „)" ელემენტებისგან. დაწერეთ ფუნქცია რომელიც აბრუნებს ფრჩხილები არის თუ არა მათემატიკურად სწორად დასმული.
 
-    for i in cents.sorted(by: > ) {
-        while sum - i >= 0 {
-            centsNeed.append(i)
-            sum -= i
-            
-            if sum == 0 {
-                break
+func isProperly(sequence: String) -> Bool {
+    var stack: [Character] = []
+    for c in sequence {
+        if c == "(" {
+            stack.append(c)
+        } else if c == ")" {
+            if stack.isEmpty || stack.popLast() != "(" {
+                return false
             }
         }
     }
-    print(centsNeed)
-    
-    return centsNeed.count
+    return stack.isEmpty
 }
 
-minSplit(120, coins)
+isProperly(sequence: "(()())(()())(())")
 
-//3.     მოცემულია მასივი, რომელიც შედგება მთელი რიცხვებისგან. დაწერეთ ფუნქცია რომელსაც გადაეცემა ეს მასივი და აბრუნებს მინიმალურ მთელ რიცხვს, რომელიც 0-ზე მეტია და ამ მასივში არ შედის.
-
-var integersArray = [1, 2, 3, 4, 5, 7, 8, 13, 15]
-
-func notContains(array: [Int]) -> Int {
-    var intArray = [Int]()
-    
-    for i in 0...array.max()! {
-        if !array.contains(i) && i > 0 {
-            intArray.append(i)
+//5. გადმოგეცემათ მთელი რიცხვი N. დაწერეთ ფუნქცია რომელიც დაითვლის რამდენი 0ით ბოლოვდება N! (ფაქტორიალი)
+//     შენიშვნა 1000! შედგება 2568 სიმბოლოსაგან.
+//
+func zeros(N: Int) -> Int {
+    var count = 0
+    for i in 1...N {
+        var num = i
+        while num % 5 == 0 {
+            count += 1
+            num /= 5
         }
     }
-    return intArray.sorted(by: <).first!
+    return count
 }
 
-notContains(array: integersArray)
+zeros(N: 7)
+//     example:
+//     zeros(N: 7) ➞ 1
+//     zeros(N: 12) ➞ 2
+//     zeros(N: 490) ➞ 120
 
-//4. მოცემულია String რომელიც შედგება „(„ და „)“ ელემენტებისგან. დაწერეთ ფუნქცია რომელიც აბრუნებს ფრჩხილები არის თუ არა მათემატიკურად სწორად დასმული.
 
-//===
-
-//5. გვაქვს n სართულიანი კიბე, ერთ მოქმედებაში შეგვიძლია ავიდეთ 1 ან 2 საფეხურით. დაწერეთ ფუნქცია რომელიც დაითვლის n სართულზე ასვლის ვარიანტების რაოდენობას.
-
-//===
-
-//6. დაწერეთ საკუთარი მონაცემთა სტრუქტურა, რომელიც საშუალებას მოგვცემს O(1) დროში წავშალოთ ელემენტი.
-
-//===
